cf0a513922944d4b08329f900baf78db4339033b
Joshua OHanlon <josh.t.ohanlon@gmail.com>
2021-01-29 22:08:51 -0600

    Added like feature to listings and things to do

--- client/components/Thing.jsx
+++ client/components/Thing.jsx
@@ -1,8 +1,75 @@
 import React from 'react';
+import styled from 'styled-components';
+
+const HeartStyle = styled.span`
+  background-color: black;
+  display: inline-block;
+  height: 12px;
+  margin: 0 10px;
+  position: relative;
+  top: 0;
+  -webkit-transform: rotate(45deg);
+  -moz-transform: rotate(45deg);
+  -ms-transform: rotate(45deg);
+  -o-transform: rotate(45deg);
+  transform: rotate(-45deg);
+  width: 16px;
+  &:before,
+  &:after {
+    content: "";
+    background-color: black;
+    border-radius: 50%;
+    height: 12px;
+    position: absolute;
+    width: 12px;
+  }
+  &:before {
+    top: -8px;
+    left: 0;
+  }
+  &:after {
+    left: 8px;
+    top: 0;
+  }
+`
+const HeartClicked = styled.span`
+  background-color: red;
+  display: inline-block;
+  height: 12px;
+  margin: 0 10px;
+  position: relative;
+  top: 0;
+  -webkit-transform: rotate(45deg);
+  -moz-transform: rotate(45deg);
+  -ms-transform: rotate(45deg);
+  -o-transform: rotate(45deg);
+  transform: rotate(-45deg);
+  width: 16px;
+  &:before,
+  &:after {
+    content: "";
+    background-color: red;
+    border-radius: 50%;
+    height: 12px;
+    position: absolute;
+    width: 12px;
+  }
+  &:before {
+    top: -8px;
+    left: 0;
+  }
+  &:after {
+    left: 8px;
+    top: 0;
+  }
+`
 
 class Thing extends React.Component {
   constructor(props) {
     super(props)
+    this.state = {
+      isClicked: false
+    }
     this.imgStyle = {
       borderRadius: '5%',
       height: '300px',
@@ -28,12 +95,32 @@ class Thing extends React.Component {
     this.descContainerStyle = {
       paddingBottom: '5px'
     }
+    this.imgContainer = {
+      position: 'relative'
+    }
+    this.heartContainer = {
+      position: 'absolute',
+      top: '10px',
+      right: '5px'
+    }
+    this.clickHandler = this.clickHandler.bind(this);
+  }
+
+  clickHandler() {
+    let isClicked = !this.state.isClicked;
+    this.setState({isClicked})
   }
 
   render() {
     return (
       <div id='listingContainerThing'>
-        <div id='imgContainerThing'>
+        <div id='imgContainerThing' style={this.imgContainer}>
+          <div style={this.heartContainer} onClick={this.clickHandler}>
+            {
+              this.state.isClicked ? <HeartClicked></HeartClicked> :
+                <HeartStyle></HeartStyle>
+            }
+          </div>
           <img src={this.props.thing.pic} style={this.imgStyle} />
         </div>
         <div id='reviewContainerThing' style={this.reviewContStyle}>
